6d2ed989847ba6be3d0827543ef5984e7ba2eef2
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-03-13 08:43:51 +0300

    Изменил метод open_onyma()

--- resources/Functions_Session_Count.py
+++ resources/Functions_Session_Count.py
@@ -43,14 +43,25 @@ def get_accounts(cursor):
     cursor.execute(command)
     return cursor.fetchall()
 
-def open_onyma(browser):
-    browser.get("https://10.144.196.37/onyma/")
-    element = browser.find_element_by_id("LOGIN")
-    element.send_keys(Settings.onyma_login)
-    element = browser.find_element_by_id("PASSWD")
-    element.send_keys(Settings.onyma_password)
-    element = browser.find_element_by_id("enter")
-    element.click()
+def open_onyma():
+    browser = get_browser()
+    while True:
+        try:
+            browser.get("https://10.144.196.37/onyma/")
+            element = browser.find_element_by_id("LOGIN")
+            element.send_keys(Settings.onyma_login)
+            element = browser.find_element_by_id("PASSWD")
+            element.send_keys(Settings.onyma_password)
+            element = browser.find_element_by_id("enter")
+            element.click()
+        except:
+            browser.quit()
+            del browser
+            time.sleep(15)
+            browser = get_browser()
+        else:
+            break
+    return browser
         
 def check_sessions(card_name,  bill,  dmid,  tmid,  date,  browser,  cursor):
     browser.get("https://10.144.196.37/onyma/main/ddstat.htms?bill={}&dt={}&mon={}&year={}&service=201&dmid={}&tmid={}".format(bill,  date.day,  date.month, date.year,  dmid,  tmid))
@@ -75,8 +86,7 @@ def check_sessions(card_name,  bill,  dmid,  tmid,  date,  browser,  cursor):
 def run(account_list):
     connect = MySQLdb.connect(host=Settings.db_host, user=Settings.db_user, password=Settings.db_password, db=Settings.db_name, charset='utf8')
     cursor = connect.cursor()
-    browser = get_browser()
-    open_onyma(browser)
+    browser = open_onyma()
 
     prev_day = datetime.date.today() - datetime.timedelta(days=1)
     for account in account_list:
@@ -90,7 +100,6 @@ def run(account_list):
             browser.quit()
             del browser
             time.sleep(15)
-            browser = get_browser()
-            open_onyma(browser)
+            browser = open_onyma()
     connect.close()
     browser.quit()
