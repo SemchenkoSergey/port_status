11d3275046570323147524707bc69513901aa416
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-03-15 09:54:18 +0300

    Добавил дополнительную проверку при count = 0. Это нужно если абонент поменял тариф после последнего обновления abon_dsl.

--- resources/Functions_Session_Count.py
+++ resources/Functions_Session_Count.py
@@ -70,6 +70,26 @@ def check_sessions(card_name,  bill,  dmid,  tmid,  date,  browser,  cursor):
         count = re.search(r'<td class="foot">Все</td><td class="pgout" colspan="5">.+?<b>(\d+)</b>',  browser.page_source.__repr__()).group(1)
     else:
         return False
+    if count == '0':
+        cur_bill, cur_tmid, cur_dmid = find_param(browser, card_name)
+        if cur_bill != bill or cur_tmid != tmid or cur_dmid != dmid:
+            command = '''
+            UPDATE abon_dsl
+            SET bill = "{}", dmid = "{}", tmid = "{}"
+            WHERE account_name = "{}"
+            '''.format(cur_bill, cur_dmid, cur_tmid, card_name)
+            try:
+                cursor.execute(command)
+            except:
+                pass
+            else:
+                cursor.execute('commit')            
+            browser.get("https://10.144.196.37/onyma/main/ddstat.htms?bill={}&dt={}&mon={}&year={}&service=201&dmid={}&tmid={}".format(cur_bill,  date.day,  date.month, date.year,  cur_dmid,  cur_tmid))
+            browser.find_element_by_id("onymaPageFooter")
+            if re.search(r'<td class="foot">Все</td><td class="pgout" colspan="5">.+?<b>(\d+)</b>',  browser.page_source.__repr__()):
+                count = re.search(r'<td class="foot">Все</td><td class="pgout" colspan="5">.+?<b>(\d+)</b>',  browser.page_source.__repr__()).group(1)
+            else:
+                return False            
     command = '''
     INSERT INTO data_sessions
     (account_name, date, count)
@@ -82,6 +102,26 @@ def check_sessions(card_name,  bill,  dmid,  tmid,  date,  browser,  cursor):
         pass
     else:
         cursor.execute('commit')
+
+def find_param(browser, card_name):
+    element = browser.find_element_by_id("menu4185")
+    element.click()
+    element = browser.find_element_by_partial_link_text(datetime.date.today().strftime('%Y'))
+    element.click()
+    elements = browser.find_elements_by_link_text(card_name)
+    find = False
+    for element in elements[::-1]:
+        if 'service=201' in element.get_attribute('href'):
+            element.click()
+            find = True
+            break
+    if not find:
+        return False
+    current_url = browser.current_url
+    bill = re.search(r'bill=(\d+)',  current_url).group(1)
+    tmid = re.search(r'tmid=(\d+)',  current_url).group(1)
+    dmid = re.search(r'dmid=(\d+)',  current_url).group(1)
+    return bill, tmid, dmid
 
 def run(account_list):
     connect = MySQLdb.connect(host=Settings.db_host, user=Settings.db_user, password=Settings.db_password, db=Settings.db_name, charset='utf8')
