44f5f29d52fb7f887e581a6d5de2b1c5db3bf3e4
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-03-18 18:37:52 +0300

    Файл настроек

---
+++ resources/Settings.py
@@ -0,0 +1,30 @@
+# coding: utf8
+
+### Port_Status ###
+# Учетные данные DSLAM
+login_5600 = ''
+password_5600 = ''
+login_5616 = ''
+password_5616 = ''
+
+#Количество запусков в день
+number_of_launches = 12
+
+#Количество потоков выполнения
+threads = 5
+
+#Список DSLAM
+hosts = (('ip', '5600'),
+         ('ip', '5616'))
+         
+# Mysql
+db_host = 'localhost'
+db_user = ''
+db_password = ''
+db_name = ''
+
+### Session_Count ###
+threads_count = 3
+# Onyma
+onyma_login = ''
+onyma_password = ''
