e3f6633f66b9132e5f7d323854334fe451f0cb0c
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-05-23 14:45:32 +0300

    Добавил уточнение скорости тарифа через ониму.

--- resources/Make_Abon_Onyma.py
+++ resources/Make_Abon_Onyma.py
@@ -71,13 +71,36 @@ def run_define_param(account_list):
     del onyma
     return count_processed
 
-
+def run_define_speed(account_list):
+    count_processed = 0
+    connect = MySQLdb.connect(host=Settings.db_host, user=Settings.db_user, password=Settings.db_password, db=Settings.db_name, charset='utf8')
+    cursor = connect.cursor()
+    onyma = Onyma.get_onyma()
+    for account in account_list:
+        account_name = account[0]
+        speed = Onyma.find_account_speed(onyma, account_name)
+        if speed is not False:
+            command = '''
+            UPDATE abon_dsl
+            SET tariff = {}
+            WHERE account_name = "{}"
+            '''.format(speed, account_name)
+            try:
+                cursor.execute(command)
+            except:
+                pass
+            else:
+                cursor.execute('commit')
+            count_processed += 1
+    connect.close()
+    del onyma
+    return count_processed
+    
 def main():
     print("Начало работы: {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     create_abon_onyma()
-    count = 0
     
-    # Заполнение полей bill, dmid, tmid
+    ## Заполнение полей bill, dmid, tmid
     account_list = get_accounts()
     if len(account_list) == 0:
         print('\n!!! Необходимо сформировать таблицу abon_dsl !!!\n')
@@ -86,7 +109,18 @@ def main():
     print('\nПолучение данных из Онимы...')
     with ThreadPoolExecutor(max_workers=Settings.threads_count) as executor:
         result = executor.map(run_define_param, arguments)
+    count = 0
     for i in result:
         count += i
+    print('\nПолучение bill, dmid, tmid')
     print('\nОбработано: {}'.format(count))
+    
+    # Уточнение тарифов
+    with ThreadPoolExecutor(max_workers=Settings.threads_count) as executor:
+        result = executor.map(run_define_speed, arguments)
+    count = 0
+    for i in result:
+        count += i
+    print('\nУточнение тарифов')
+    print('\nОбработано: {}'.format(count))    
     print("\nЗавершение работы: {}".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
--- resources/Onyma.py
+++ resources/Onyma.py
@@ -64,3 +64,44 @@ def find_account_param(onyma, account_name):
         return bill, dmid, tmid
     else:
         return False
+
+def get_speed(tariff):
+    re_speed_kb = re.compile(r'(\d+) ?[k|К]')
+    re_speed_mb = re.compile(r'(\d+) ?Мбит')
+    
+    if re_speed_mb.search(tariff):
+        speed = int(re_speed_mb.search(tariff).group(1)) * 1024
+    elif re_speed_kb.search(tariff):
+        speed = int(re_speed_kb.search(tariff).group(1))
+    else:
+        return False
+    return speed
+    
+def find_account_speed(onyma, account_name):
+    url_ip = 'https://10.144.196.37'
+    url_main = 'https://10.144.196.37/onyma/main/'
+    try:
+        html = onyma.post('https://10.144.196.37/onyma/main/dogsearch_ok.htms', {'sitename': account_name, 'search': 'Поиск'}, verify=False).text
+        url = BS(html, 'lxml').find('a', title='Персональный тариф').get('href')
+        html = onyma.get(url_ip + url).text
+        tds = BS(html, 'lxml').find_all('td', class_='td1')
+    except:
+        return -1
+    rows = int(len(tds)/6)
+    speed = 0
+    tariffs = []
+    for row in range(0, rows):
+        tariffs.append((tds[row * 6].text.strip(), tds[row * 6 + 1].text.strip(), tds[row*6 + 4].text.strip()))
+    for tariff in tariffs:
+        if tariff[2] == '':
+            for i in (0,1):
+                cur_speed = get_speed(tariff[i])
+                if cur_speed is not False:
+                    if cur_speed > speed:
+                        speed = cur_speed
+                        if i == 1:
+                            print('{} - скорость ТП {}'.format(account_name, speed))
+    if speed > 0:
+        return speed
+    else:
+        return False
