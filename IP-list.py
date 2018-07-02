2d44883ee9874cd7ae7427677898e9e18ba7e579
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-07-02 16:46:22 +0300

    Выдает по 20 ip для отчетов из Аргуса

---
+++ IP-list.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from resources import Settings
+
+def main():
+    out = ''
+    count = 0
+    for host in Settings.hosts:
+        if count % 20 == 0 and count != 0:
+            print(out)
+            out = ''
+        out += host[0] + ';' 
+        count += 1
+    print(out)
+
+
+if __name__ == '__main__':
+    main()
