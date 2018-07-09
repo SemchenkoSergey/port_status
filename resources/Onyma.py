022c5822af314ffb12a14793fc73d0504af63f73
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-07-09 16:26:28 +0300

    Добавил тарифы "Нон-стоп" и "Игровой"

--- resources/Onyma.py
+++ resources/Onyma.py
@@ -102,6 +102,10 @@ def get_speed(tariff):
         speed = int(re_speed_kb.search(tariff).group(1))
     elif 'Максимальная скорость'.lower() in tariff.lower():
         return 15 * 1024
+    elif 'Нон-стоп max'.lower() in tariff.lower():
+        return 15 * 1024
+    elif 'тариф "Игровой"'.lower() in tariff.lower():
+        return 15 * 1024    
     else:
         return False
     return speed
